22888492d95f827073b50528187e9b51fdd0fe4a
Antonio <antonioluiscallaucano@gmail.com>
2024-03-03 18:13:06 +0100

    v.1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const dotenv = require('dotenv')
+dotenv.config()
+
+const fs = require('fs')
+
+let usuarios_json = fs.readFileSync('./usuarios.json', 'utf-8')
+let usuarios_obj = JSON.parse(usuarios_json)
+
+// let peliculas_json = fs.readFileSync('./peliculas.json', 'utf-8')
+// let peliculas_obj = JSON.parse(peliculas_json)
+
+const bcryptjs = require('bcryptjs')
+
+// -- Funciones --
+let guardar_JSON_users = () => {
+  try {
+    fs.writeFileSync('./usuarios.json', JSON.stringify(usuarios_obj), 'utf-8')
+  } catch (error) {
+    console.log(error)
+  }
+}
+// let guardar_JSON_productos = () => {
+//   try {
+//     fs.writeFileSync('./peliculas.json', JSON.stringify(usuarios_obj), 'utf-8')
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// -- ROUTER --
+router.get('/', (req, res) => {
+  res.render('landing')
+})
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
 
 module.exports = router;
